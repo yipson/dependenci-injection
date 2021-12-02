40712440195fac5c192fdbb7d09ed119ae732e44
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-02 16:24:13 -0500

    DEFAULT PROFILE * La configuracion que nos permite realizar profile nos abre las puertas a poder definir que clases seran usados en cada ambiente de despliegue en tiempo de ejecucion * Aun, si no tenemos una configuracion en application.properties para profile, podemos, por medio de las anotaciones @Profile({'ENVIRONTEMT', 'default'}), definir cuales seran las clases que estaran en el contexto de spring

--- src/main/java/guru/springframework/di/services/I18nSpanishService.java
+++ src/main/java/guru/springframework/di/services/I18nSpanishService.java
@@ -3,7 +3,10 @@ package guru.springframework.di.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+/* Esta clase sera la que se use,
+* asi no se haya configurado spring.profiles.active
+* ya que que la anotacion @Profile cuenta con el valor default */
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishService implements GreetingService {
 
