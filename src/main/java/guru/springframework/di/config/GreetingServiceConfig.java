2d12de99092640ef6573ac3a3baa477a46eb3f23
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-07 16:28:49 -0500

    Agregando varibales ambiente por medio de los archivos .properties, el ambiente en el que se va a ejecutar el programa se define por medio de la propiedad spring.profiles.active, definida en application.properties (la misma propiedad con la que definimos el profile de los beans a utilizar)

--- src/main/java/guru/springframework/di/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/di/config/GreetingServiceConfig.java
@@ -9,7 +9,7 @@ import guru.springframework.di.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
+
 @ImportResource("classpath:di-config.xml")
 @Configuration
 public class GreetingServiceConfig {
